38f62ed3767f7fb03192d36c6a4149ad8d5a3b90
josdem <joseluis.delacruz@gmail.com>
2018-01-25 21:45:22 -0600

    Finishing list all persons method

--- src/main/groovy/com/jos/dem/springboot/restful/controller/PersonController.groovy
+++ src/main/groovy/com/jos/dem/springboot/restful/controller/PersonController.groovy
@@ -1,7 +1,10 @@
 package com.jos.dem.springboot.restful.controller
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 import com.jos.dem.springboot.restful.model.Person
 
@@ -9,7 +12,8 @@ import com.jos.dem.springboot.restful.model.Person
 @RequestMapping('/persons/**')
 class PersonController {
 
-  @RequestMapping(consumes='application/json')
+  @RequestMapping(method=GET)
+  @ResponseBody
   List<Person> getPersons(){
     Person person = new Person(nickname:'josdem', email:'joseluis.delacruz@gmail.com')
     List<Person> persons = []
