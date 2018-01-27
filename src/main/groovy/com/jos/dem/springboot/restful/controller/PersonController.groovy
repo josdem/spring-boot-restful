2ffbc6be667a9012f72fd4bd90deabf692eade68
josdem <joseluis.delacruz@gmail.com>
2018-01-26 20:17:05 -0600

    Deleting unused class

--- src/main/groovy/com/jos/dem/springboot/restful/controller/PersonController.groovy
+++ src/main/groovy/com/jos/dem/springboot/restful/controller/PersonController.groovy
@@ -5,15 +5,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.ResponseEntity
-import org.springframework.http.HttpStatus
 
 import com.jos.dem.springboot.restful.service.PersonService
 import com.jos.dem.springboot.restful.model.Person
-import com.jos.dem.springboot.restful.exception.BusinessException
 
 @RestController
 @RequestMapping('/persons/**')
@@ -26,12 +21,6 @@ class PersonController {
   @ResponseBody
   List<Person> getPersons(){
     personService.getPersons()
-  }
-
-  @ResponseStatus(value=HttpStatus.UNAUTHORIZED)
-  @ExceptionHandler(BusinessException.class)
-  ResponseEntity<String> handleException(BusinessException be){
-    return new ResponseEntity<String>(be.message, HttpStatus.UNAUTHORIZED)
   }
 
 }
